685b8d3a122e35f3a8fb5818612e90a64f79d02f
hazydodo <dodoproductions33@gmail.com>
2021-08-12 05:12:11 +0200

    better commends

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::io;
 
 fn main() {
 
+    // create a new empty mutable string
     let mut guess = String::new();
 
 
-    // get guess input
+    // get user input 
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read input");
 
     
+    // trim the string of unwanted characters
     let guess = guess.trim();
     
     println!("is the string a palindrome?: {}", is_palindrome(&guess));
@@ -18,7 +20,6 @@ fn main() {
 
 fn is_palindrome(guess: &str) -> bool {
     for index in 0..(guess.len() / 2){
-        //println!("{}", index);
         if &guess[index..index + 1] == &guess[guess.len() - (1 + index)..guess.len() - index] {
             continue;
         }
@@ -29,4 +30,3 @@ fn is_palindrome(guess: &str) -> bool {
     true 
 }
 
-// hello
